ea17f34cc621e5eb9fd8a66d9222fe9b94bddc2b
Pseurae <adhith.chand@gmail.com>
2022-05-05 08:56:07 +0530

    Push Chibi and Snake

---
+++ Chibi/chibi.py
@@ -0,0 +1,214 @@
+"""
+Copyright 2022 Adhith Chand Thiruvath (Pseurae)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+# A Chibi widget that can be punted across the screen and
+# interacts on click.
+
+import pygame
+import pygame.draw
+import pygame.time
+import pygame.display
+import pygame.image
+import random
+import time
+import math
+
+background_colour = (255, 255, 255)
+(width, height) = (400, 400)
+drag = 0.999
+elasticity = 0.55
+gravity = (0.0, 0.2)
+
+target_fps = 60
+
+def sign(x): return -1 if x < 0 else 1
+
+idle_image = pygame.image.load("m_sticker_1.png")
+idle_image_flipped = pygame.transform.flip(idle_image, True, False)
+
+hover_image = pygame.image.load("m_sticker_2.png")
+hover_image_flipped = pygame.transform.flip(hover_image, True, False)
+
+class Chibi(object):
+    def random_jump_delay(self):
+        return random.random() * 4 + 4
+
+    def __init__(self, x, y, mass):
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.mass_inv = 1.0 / mass
+
+        self.speed_x = 0
+        self.speed_y = 0
+
+        self.width = max(idle_image.get_width(), hover_image.get_width())
+        self.height = max(idle_image.get_height(), hover_image.get_height())
+
+        self.jump_delay = self.random_jump_delay()
+        self.flipped = False # True: Left, False: Right
+        self.jump_move = 0
+
+    def interact(self, st):
+        if self.hovered() or self.y > height - self.height:
+            self.jump_delay = st + self.random_jump_delay()
+
+        if self.jump_delay < st:
+            if self.jump_move > 0:
+                self.jump_move = random.randint(-1, 0)
+            elif self.jump_move < 0:
+                self.jump_move = random.randint(0, 1)
+            else:
+                self.jump_move = random.randint(-1, 1)
+
+            self.flipped = True if self.jump_move > 0 else False
+
+            self.speed_x = 1.0 * self.jump_move
+            self.speed_y = -3.0
+
+            self.jump_delay = st + self.random_jump_delay()
+
+    def hovered(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        return all([
+            mouse_x > self.x,
+            mouse_x < self.x + self.width,
+            mouse_y > self.y,
+            mouse_y < self.y + self.height
+        ])
+
+    @property
+    def idle_image(self):
+        if self.flipped:
+            return idle_image_flipped
+
+        return idle_image
+
+    @property
+    def hover_image(self):
+        if self.flipped:
+            return hover_image_flipped
+
+        return hover_image
+
+    @property
+    def image(self):
+        if self.hovered():
+            return self.hover_image
+
+        return self.idle_image
+
+    def render_step(self, screen):
+        screen.blit(self.image, (int(self.x), int(self.y)))
+
+    def physics_step(self, gravity, drag, dt):
+        def modulate_position(x, y):
+            self.x += x * dt
+            self.y += y * dt
+
+        def modulate_velocity(x, y):
+            self.speed_x += x * dt
+            self.speed_y += y * dt
+
+        modulate_velocity(*gravity)
+        modulate_position(self.speed_x, self.speed_y)
+
+        self.speed_x *= drag
+        self.speed_y *= drag
+
+    def bounce_step(self, restitution):
+        def restitute_speed(xsign, ysign):
+            self.speed_x = xsign * sign(self.speed_x) * abs(self.speed_x) * restitution
+            self.speed_y = ysign * sign(self.speed_y) * abs(self.speed_y) * restitution
+
+        if self.x > width - self.width:
+            self.x = width - self.width
+            restitute_speed(-1, 1)
+
+        elif self.x < 0:
+            self.x = 0
+            restitute_speed(-1, 1)
+
+        if self.y > height - self.height:
+            self.y = height - self.height
+            restitute_speed(1, -1)
+
+        elif self.y < 0:
+            self.y = 0
+            restitute_speed(1, -1)
+
+g_chibi = Chibi(200, 200, 3.0)
+g_chibi.speed_x = 2.0 * (random.random() * 2.0 - 1.0)
+g_chibi.speed_y = 2.0 * (random.random() * 2.0 - 1.0)
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    proggy = pygame.font.Font('ProggyVector Regular.ttf', 12)
+
+    window = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+    start_time = None
+
+    pygame.display.set_caption('Chibi Physics')
+
+    is_running = True
+    selected_chibi = False
+
+    while is_running:
+        dt = clock.tick(target_fps) * 0.001 * target_fps
+        (mouse_x, mouse_y) = pygame.mouse.get_pos()
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                is_running = False
+
+            elif ev.type == pygame.MOUSEBUTTONDOWN:
+                selected_chibi = g_chibi.hovered()
+
+            elif ev.type == pygame.MOUSEBUTTONUP:
+                selected_chibi = False
+
+        if selected_chibi:
+            dx = mouse_x - g_chibi.x - (g_chibi.width / 2.0)
+            dy = mouse_y - g_chibi.y - (g_chibi.height / 2.0)
+
+            g_chibi.speed_x = dx * 0.1
+            g_chibi.speed_y = dy * 0.1
+
+        window.fill(background_colour)
+
+        position_string = "Bounding Box: {:.02f}, {:.02f}, {:.02f}, {:.02f}".format(g_chibi.x, g_chibi.y, g_chibi.x + g_chibi.width, g_chibi.y + g_chibi.height)
+        velocity_string = "Velocity: {:.02f}, {:.02f}".format(g_chibi.speed_x, g_chibi.speed_y)
+        delta_t_string = "Delta Time: {:.02f}".format(dt)
+
+        def text_render(screen, *arg):
+            y = 0
+
+            for t in arg:
+                text_surf = proggy.render(t, False, (0, 0, 0), None)
+                screen.blit(text_surf, (0, y))
+                y += text_surf.get_height()
+
+        text_render(window, position_string, velocity_string, delta_t_string)
+
+        g_chibi.physics_step(gravity, drag, min(dt, 1.0))
+        g_chibi.bounce_step(elasticity)
+
+        if start_time is None:
+            start_time = time.time()
+
+        g_chibi.render_step(window)
+        g_chibi.interact(time.time() - start_time)
+
+        pygame.display.flip()
+
+    pygame.quit()
---
+++ Snake/snake.py
@@ -0,0 +1,251 @@
+"""
+Copyright 2022 Adhith Chand Thiruvath (Pseurae)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+# May have bugs. Not gonna fix them as, lore-wise, Jez creates this.
+# Also was hastily coded during a power outage and with less than 10%
+# of battery backup. Not gonna fix that either.
+
+from random import randrange
+import colorsys
+import pygame
+import pygame.draw
+import pygame.time
+import pygame.display
+import math
+
+game_over_tail_bite = False
+game_over_out_of_bounds = True
+
+FaOREGROUND = 0
+RANDOM = 1
+GRADIENT = 2
+
+snake_color_option = FOREGROUND
+fruit_color_option = RANDOM
+
+foreground_color = (0, 0, 0)
+grid_outline_color = (200, 200, 200)
+background_colour = (255, 255, 255)
+
+grid_line_width = 1
+grid_size = 20
+xcells = 20
+ycells = 20
+
+width = xcells * grid_size + (xcells - 1) * grid_line_width
+height = ycells * grid_size + (ycells - 1) * grid_line_width
+
+target_fps = 5
+
+def generate_gradients():
+    rv = [ ]
+
+    for y in range(ycells):
+        rrv = [ ]
+
+        for x in range(xcells):
+            theta = math.radians(-45.0)
+            norm_x = x / (xcells - 1.0)
+            norm_y = y / (ycells - 1.0)
+
+            hue = norm_x * math.cos(theta) - norm_y * math.sin(theta)
+            r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+
+            rrv.append((
+                int(r * 255),
+                int(g * 255),
+                int(b * 255)
+            ))
+
+        rv.append(rrv)
+
+    return rv
+
+gradient_colors = generate_gradients()
+
+class Block(object):
+    def __init__(self, x, y, padding, color):
+        self.x = x
+        self.y = y
+        self.padding = padding
+        self.color = color
+
+    def __repr__(self):
+        return "<Block x: {self.x} y: {self.y}>".format(self=self)
+
+    def place(self, renderer):
+        return renderer.cell(self.x, self.y, self.color, self.padding)
+
+class Renderer(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((width, height))
+
+    def __getattr__(self, name):
+        if hasattr(self.window, name):
+            return getattr(self.window, name)
+        else:
+            super(Renderer, self).__getattr__(name)
+
+    def grid(self, color):
+        for i in range(1, xcells):
+            x = i * grid_size + (i - 1) * grid_line_width
+            pygame.draw.line(self.window, color, (x, 0), (x, self.height), grid_line_width)
+
+        for i in range(1, ycells):
+            y = i * grid_size + (i - 1) * grid_line_width
+            pygame.draw.line(self.window, color, (0, y), (self.width, y), grid_line_width)
+
+    def cell(self, x, y, color=foreground_color, padding=(0, 0)):
+        if isinstance(padding, (float, int)):
+            xpadding = ypadding = padding
+        else:
+            xpadding, ypadding = padding
+
+        x0 = x * (grid_size + grid_line_width)
+        y0 = y * (grid_size + grid_line_width)
+
+        rect = pygame.Rect(x0 + xpadding, y0 + xpadding, grid_size - 2 * xpadding, grid_size - 2 * ypadding)
+        return pygame.draw.rect(self.window, color, rect)
+
+def random_color():
+    return (randrange(256), randrange(256), randrange(256))
+
+def dep_color(option, x, y):
+    if option == FOREGROUND:
+        return foreground_color
+    elif option == GRADIENT:
+        return gradient_colors[y][x]
+    elif option == RANDOM:
+        return random_color()
+
+    return option
+
+def snake_color(x, y):
+    return dep_color(snake_color_option, x, y)
+
+def fruit_color(x, y):
+    return dep_color(fruit_color_option, x, y)
+
+def default_snake_body():
+    xcenter = xcells // 2
+    ycenter = ycells // 2
+
+    rv = [ ]
+
+    for i in range(4):
+        y = ycenter + i
+        rv.append(Block(
+            xcenter,
+            y,
+            0,
+            snake_color(xcenter, y)
+        ))
+
+    return rv
+
+def create_fruit():
+    x = randrange(1, xcells - 1)
+    y = randrange(1, ycells - 1)
+    return Block(x, y, 5, fruit_color(x, y))
+
+snake_body = default_snake_body()
+
+class Direction(object):
+    Up = (0, -1)
+    Down = (0, 1)
+    Left = (-1, 0)
+    Right = (1, 0)
+
+direction_keys = {
+    pygame.K_UP: Direction.Up,
+    pygame.K_DOWN: Direction.Down,
+    pygame.K_LEFT: Direction.Left,
+    pygame.K_RIGHT: Direction.Right,
+}
+
+opposites = {
+   Direction.Up: Direction.Down,
+   Direction.Down: Direction.Up,
+   Direction.Left: Direction.Right,
+   Direction.Right: Direction.Left
+}
+
+if __name__ == "__main__":
+    pygame.init()
+
+    proggy = pygame.font.Font('ProggyVector Regular.ttf', 12)
+
+    renderer = Renderer(width, height)
+    clock = pygame.time.Clock()
+    start_time = None
+
+    pygame.display.set_caption('Snek')
+
+    is_running = True
+
+    current_direction = Direction.Up
+    current_position = [ snake_body[0].x, snake_body[0].y ]
+
+    fruit = create_fruit()
+
+    while is_running:
+        direction_change_to = current_direction
+
+        dt = clock.tick(target_fps) * 0.001 * target_fps
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                is_running = False
+
+            elif ev.type == pygame.KEYDOWN:
+                direction_change_to = direction_keys.get(ev.key, direction_change_to)
+
+        if opposites[current_direction]!= direction_change_to:
+            current_direction = direction_change_to
+
+        current_position[0] += current_direction[0]
+        current_position[1] += current_direction[1]
+
+        x, y = current_position[0], current_position[1]
+        new_block = Block(x, y, 0, snake_color(x, y))
+        snake_body.insert(0, new_block)
+
+        if x == fruit.x and y == fruit.y:
+            fruit = create_fruit()
+        else:
+            snake_body.pop()
+
+        if game_over_tail_bite:
+            for b in snake_body[1:]:
+                if x == b.x and y == b.y:
+                    is_running = False
+
+        if game_over_out_of_bounds:
+            is_out_of_bounds = any([
+                x < 0, x > xcells - 1,
+                y < 0, y > ycells - 1
+            ])
+
+            if is_out_of_bounds:
+                is_running = False
+
+        # Render step.
+        renderer.fill(background_colour)
+        renderer.grid(grid_outline_color)
+
+        fruit.place(renderer)
+        for b in snake_body:
+            b.place(renderer)
+
+        pygame.display.flip()
+
+    pygame.quit()
